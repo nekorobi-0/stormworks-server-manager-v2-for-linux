09bbac12b3ab1e38bda4a6095feb6b7c194acfa8
nekorobi_0 <65654766+nekorobi-0@users.noreply.github.com>
2023-11-15 14:08:08 +0900

    Update class_server.py

--- class_server.py
+++ class_server.py
@@ -14,6 +14,7 @@ class server:
         with open(f"stw/sv{self.dir}/server_config.xml","w")as f:
             f.write(prof)
         self.RunningProfile = profile
+        print(f"start sv{str(self.dir)}.exe +server_dir sv{str(self.dir)}")
         self.server = subprocess.Popen(f"start sv{str(self.dir)}.exe +server_dir sv{str(self.dir)}",
             cwd=r"C:\stormworks-server-manager-v2-for-linux\stw")
         #self.server = subprocess.Popen(f"sv_fake.bat",shell=True)
